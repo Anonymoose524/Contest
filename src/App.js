46af770879e24b4220591cf85641236dd40f47ae
Anonymous524 <anonymousl524l@gmail.com>
2021-11-12 21:10:25 -0800

    Move to pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import Home from "./components/Home";
-import Contests from "./components/Contests";
+import Home from "./pages/Home";
+import Contests from "./pages/Contests";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Routes>
-        <Route path = "/" element = {<Home/>} />
-        <Route path = "/contests" element = {<Contests/>} />
+        <Route exact path = "/" element = {<Home/>} />
+        <Route exact path = "/contests" element = {<Contests/>} />
+        <Route exact path = "/admin" element = {<Admin/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Contests.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contests(){
+    return (
+        <div className = "container">
+            <br></br>
+            <h1 className="page-header">Contests</h1>
+            <hr></hr>
+        </div>
+    );
+}
+
+export default Contests;
