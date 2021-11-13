fd0a153a8ebdcf45611650452a981bba9cc7be82
Anonymous524 <anonymousl524l@gmail.com>
2021-11-12 21:10:59 -0800

    Update navigation
    Switch to bootstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,34 @@
 import React from "react";
-import {  Link } from "react-router-dom";
-import "./Navigation.css";
+import Login from "./Login";
+//import {  Link } from "react-router-dom";
+//import "./Navigation.css";
 
 function Navigation(){
     return (
-        <div className = "navigation">
-            <ul>
-                <li style = {{float: "left"}}>
-                    <Link to="/">NPC</Link>
-                </li>
-                <li>
-                    <Link to="/contests">Contests</Link>
-                </li>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-            </ul>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="/">Newport Programming Contest</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contests">Contests</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/admin">FAQ</a>
+                        </li>
+                        <li className="nav-item">
+                            <a data-target="#myModal" data-toggle="modal" className="nav-link" href="#myModal">Logout</a> {/*TODO*/}
+                            
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </nav>
+        
     );
 }
 
