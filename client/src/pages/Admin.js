fd56f8a0d4fb7642b34a2c5f7afd8679297872ed
Anonymous524 <anonymousl524l@gmail.com>
2021-11-14 21:40:20 -0800

    update directories

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Admin(){
+    return (
+        <div className = "container">
+            <br></br>
+            <h1 className="page-header">Admin Portal</h1>
+            <hr></hr>
+            
+        </div>
+    );
+}
+
+export default Admin;
