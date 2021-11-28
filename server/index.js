a3565043cc7396991df30ee10277ff3b489d706d
Anonymous524 <anonymousl524l@gmail.com>
2021-11-27 22:15:23 -0800

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 //const bodyParser = require("body-parser");
 require('dotenv').config();
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
