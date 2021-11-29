0b9a66e7fcdd3edca73eeca723a60865350d285b
Anonymous524 <anonymousl524l@gmail.com>
2021-11-28 17:19:08 -0800

    Proper password hashing

--- server/models/account.js
+++ server/models/account.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const AccountSchema = mongoose.Schema({
     username: {
@@ -7,16 +8,32 @@ const AccountSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: false
     },
     members: {
         type: [String],
         required: true
     },
+    admin: {
+        type: Boolean,
+        default: false
+    },
     date: {
         type: Date,
         default: Date.now
     },
+    hash: String,
+    salt: String
 });
+
+AccountSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+};
+
+AccountSchema.methods.validPassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.hash === hash;
+};
 
 module.exports = mongoose.model("Account", AccountSchema);
--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -2,6 +2,54 @@ const express = require("express");
 const router = express.Router();
 const Account = require("../models/account");
 
+//Account login
+router.post("/login", async (req, res) => {
+    Account.findOne({username: req.body.username}, (err, account) => {
+        if(account === null) {
+            return res.status(400).send({
+                message: "Username does not exist"
+            });
+        } else {
+            if (account.validPassword(req.body.password)) {
+                return res.status(201).send({
+                    message : "Logged In"
+                });
+            } else {
+                return res.status(400).send({
+                    message : "Wrong Password"
+                });
+            }
+        }
+    })
+});
+
+//Account signup 
+router.post("/signup", async (req, res) => {
+    
+    if(await (Account.exists({username: req.body.username}))){
+        res.status(400).send({
+            message: "Username taken"
+        });
+    } else {
+        const newAccount = new Account({
+            username: req.body.username,
+            members: req.body.members
+        });
+        newAccount.setPassword(req.body.password);
+        newAccount.save((err, account) => {
+            if(err){
+                return res.status(400).send({
+                    message: err
+                });
+            } else {
+                return res.status(201).send({
+                    message: "Account added successfully"
+                });
+            }
+        });
+    }
+});
+
 //Get an account or get all accounts
 router.get("/", async (req, res) => {
     try {
@@ -13,7 +61,7 @@ router.get("/", async (req, res) => {
                 res.json(accounts);
             //Username doesn't exist, return 400 error
             } else {
-                res.status(400).send("Username doesn't exist");
+                res.status(400).send("Username does not exist");
             }
         //No username query, return all accounts
         } else {
@@ -35,11 +83,11 @@ router.get("/:postID", async (req, res) => {
     }
 });
 
-//Add an account
+//Add an account without hashing, only for testing
 router.post("/", async (req, res) => {
     const account = new Account({
         username: req.body.username,
-        password: req.body.password, //Remember to encrypt password
+        password: req.body.password, //Password not encrypted
         members: req.body.members
     });
     try {
@@ -57,7 +105,7 @@ router.post("/", async (req, res) => {
 router.delete("/:id", (req, res) => {
     Account.findByIdAndDelete(req.params.id, (err, docs) => {
          if(err || docs == null){
-            res.status(400).send("Username doesn't exist");
+            res.status(400).send("Username does not exist");
          } else {
             res.json(docs);
          }
