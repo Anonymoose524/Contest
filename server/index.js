6fff1d6b120b4b6fa4f1a071a1a94ca70b6ebe42
Anonymous524 <anonymousl524l@gmail.com>
2021-12-23 20:15:57 -0800

    Bodyparser unnecessary

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
-app.use(express.json());
+//app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.json())
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 
 //Import Routes
 const accountsRoute = require("./routes/accounts");
