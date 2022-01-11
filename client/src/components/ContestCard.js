256ae0f0f390c7f6109bc7de950b9d1a34e0494b
Anonymous524 <anonymousl524l@gmail.com>
2022-01-11 00:06:08 -0800

    Create the contest page for listing out all contests

---
+++ client/src/components/ContestCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Moment from "moment";
+
+function ContestCard(props) {
+    return (
+        <div className="card" style={{paddingBottom: 10, marginBottom: 10}}>
+            <div className="card-body">
+                <h3 className="card-title">{props.title}</h3>
+                <h4 className="card-subtitle text-muted">{
+                    (new Moment(props.start)).format("MMMM Do YYYY, h:mm:ss a") + " - " + 
+                        (new Moment(props.end)).format("MMMM Do YYYY, h:mm:ss a")
+                }</h4>
+                <p className="card-text">{props.description || ""}</p>
+            </div>
+        </div>
+    );
+}
+
+export default ContestCard;
--- client/src/pages/Contests.js
+++ client/src/pages/Contests.js
@@ -1,13 +1,69 @@
 import React from "react";
+import ContestCard from "../components/ContestCard";
 
-function Contests(){
-    return (
-        <div className = "container">
-            <br></br>
-            <h1 className="page-header">Contests</h1>
-            <hr></hr>
-        </div>
-    );
+class Contests extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = JSON.parse(window.localStorage.getItem('contests')) || {
+            Contests: []
+        };
+    }
+
+    setState(state) {
+        window.localStorage.setItem('contests', JSON.stringify(state));
+        super.setState(state);
+    }
+
+    async componentDidMount() {
+        //Setup contests
+        await fetch(process.env.REACT_APP_SERVER + "/contests/")
+            .then((res) => res.json())
+            .then((data) => {
+                data[0].start = new Date(data[0].start);
+                data[0].end = new Date(data[0].end);
+                this.setState({Contests: data});
+            }).catch((err) => console.log(err));
+        //Begin long polling
+        this.getContests();
+    }
+
+    //Long polling for contests
+    async getContests() {
+        await fetch(process.env.REACT_APP_SERVER + "/contests/long")
+            .then((res) => res.json())
+            .then((data) => {
+                data[0].start = new Date(data[0].start);
+                data[0].end = new Date(data[0].end);
+                this.setState({Contests: data});
+                this.getContests();
+            })
+            .catch((err) => {
+                console.log(err);
+                this.getContests();
+            });
+    }
+
+    render() {
+        return (
+            <div className = "container">
+                <br></br>
+                <h1 className="page-header">Contests</h1>
+                <hr></hr>
+                {
+                    this.state.Contests.map((contest) => (
+                        <ContestCard
+                            title={contest.title} 
+                            start={contest.start}
+                            end={contest.end}
+                            description={contest.description}
+                        />
+                    ))
+                }
+            </div>
+        );
+    }
+
 }
 
 export default Contests;
