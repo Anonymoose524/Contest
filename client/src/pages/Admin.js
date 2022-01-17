06d203520686f477df7746ab79da6307ca4d0208
Anonymous524 <anonymousl524l@gmail.com>
2022-01-17 00:30:57 -0800

    Add the announcement modal to the admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,14 +1,52 @@
 import React from "react";
+import AnnouncementModal from "../components/admin/AnnouncementModal";
 
-function Admin(){
-    return (
-        <div className = "container">
-            <br></br>
-            <h1 className="page-header">Admin Portal</h1>
-            <hr></hr>
-            
-        </div>
-    );
+class Admin extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            Announcements: [],
+            Contests: [],
+            Accounts: []
+        }
+    }
+
+    async componentDidMount() {
+        //Setup announcements
+        await fetch(process.env.REACT_APP_SERVER + "/announcements/")
+            .then((res) => res.json())
+            .then((data) => this.setState({Announcements: data}))
+            .catch((err) => console.log(err));
+        //Begin long polling
+        this.getAnnouncements();
+    }
+
+    //Long polling for announcements
+    async getAnnouncements() {
+        await fetch(process.env.REACT_APP_SERVER + "/announcements/long")
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({Announcements: data});
+                this.getAnnouncements();
+            })
+            .catch((err) => {
+                console.log(err);
+                this.getAnnouncements();
+            });
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <br></br>
+                <h1 className="page-header">Admin Portal</h1>
+                <hr></hr>
+                <AnnouncementModal Announcements={this.state.Announcements}/>
+            </div>
+        );
+    }
+    
 }
 
 export default Admin;
