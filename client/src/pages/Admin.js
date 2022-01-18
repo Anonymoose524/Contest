7b85c6d3d1d5f6a2393fee911895af9edfe210d2
Anonymous524 <anonymousl524l@gmail.com>
2022-01-17 20:39:47 -0800

    Add ContestModal to admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AnnouncementModal from "../components/admin/AnnouncementModal";
+import ContestModal from "../components/admin/ContestModal";
 
 class Admin extends React.Component {
 
@@ -18,8 +19,14 @@ class Admin extends React.Component {
             .then((res) => res.json())
             .then((data) => this.setState({Announcements: data}))
             .catch((err) => console.log(err));
+        //Setup contests
+        await fetch(process.env.REACT_APP_SERVER + "/contests/")
+            .then((res) => res.json())
+            .then((data) => this.setState({Contests: data}))
+            .catch((err) => console.log(err));
         //Begin long polling
         this.getAnnouncements();
+        this.getContests();
     }
 
     //Long polling for announcements
@@ -36,6 +43,21 @@ class Admin extends React.Component {
             });
     }
 
+    //Long polling for contests
+    async getContests() {
+        await fetch(process.env.REACT_APP_SERVER + "/contests/long")
+            .then((res) => res.json())
+            .then((data) => {
+                //TODO: Force update if shallow equality
+                this.setState({Contests: data});
+                this.getContests();
+            })
+            .catch((err) => {
+                console.log(err);
+                this.getContests();
+            });
+    }
+
     render(){
         return (
             <div className="container">
@@ -43,6 +65,7 @@ class Admin extends React.Component {
                 <h1 className="page-header">Admin Portal</h1>
                 <hr></hr>
                 <AnnouncementModal Announcements={this.state.Announcements}/>
+                <ContestModal Contests={this.state.Contests}/>
             </div>
         );
     }
