1a29c3e6181b9afda88e5b173d26e5c14edd7e59
Anonymous524 <anonymousl524l@gmail.com>
2022-06-22 17:03:28 -0700

    Enforce timeframe on contests cards
    Links in contest cards are only accessible if the timeframe is currently between the start and end times.
    One issue is directly accessing a contest by using its link.
    Area of improvement is giving admins access to contest pages even outside of the timeframe.

--- client/src/components/cards/ContestCard.js
+++ client/src/components/cards/ContestCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Moment from "moment";
+import moment from "moment";
 
 function ContestCard(props) {
     return (
@@ -7,12 +7,16 @@ function ContestCard(props) {
             <div className="card-body">
                 <h3 className="card-title">{props.title}</h3>
                 <h4 className="card-subtitle text-muted">{
-                    (new Moment(props.start)).format("MMMM Do YYYY, h:mm:ss a") + " - " + 
-                        (new Moment(props.end)).format("MMMM Do YYYY, h:mm:ss a")
+                    (moment(props.start)).format("MMMM Do YYYY, h:mm:ss a") + " - " + 
+                        (moment(props.end)).format("MMMM Do YYYY, h:mm:ss a")
                 }</h4>
                 <p className="card-text">{props.description || ""}</p>
             </div>
-            <a href={window.location.href + "/" + props.contestId} className="stretched-link"/>
+            {
+                (moment().isBefore(moment(props.start)) || moment().isAfter(moment(props.end))) ? 
+                    <></> : 
+                    <a href={window.location.href + "/" + props.contestId} className="stretched-link"/>
+            }
         </div>
     );
 }
