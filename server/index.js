687216a2a7d4c6314f2f3f3774b3952d354ad4c4
Anonymous524 <anonymousl524l@gmail.com>
2022-06-23 14:49:05 -0700

    Remove unnecessary variables

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-//const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const app = express();
@@ -9,21 +8,14 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
-//app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(bodyParser.json())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
 //Import Routes
-const accountsRoute = require("./routes/accounts");
-app.use("/accounts", accountsRoute);
-
-const announcementsRoute = require("./routes/announcements");
-app.use("/announcements", announcementsRoute);
-
-const contestsRoute = require("./routes/contests");
-app.use("/contests", contestsRoute);
+app.use("/accounts", require("./routes/accounts"));
+app.use("/announcements", require("./routes/announcements"));
+app.use("/contests", require("./routes/contests").default);
+app.use("/submissions", require("./routes/submissions"));
 
 app.get("/", (req, res) => {
     res.send("Home page");
